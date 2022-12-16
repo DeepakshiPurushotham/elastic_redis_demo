41541a8f1609306cfaad3a3b15c5f980b866d47f
DeepakshiPurushotham <107757971+DeepakshiPurushotham@users.noreply.github.com>
2022-12-16 16:20:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -82,7 +82,7 @@ locals {
 
 resource "aws_elasticache_subnet_group" "default" {
   count       = module.this.enabled && var.elasticache_subnet_group_name == "" && length(var.subnets) > 0 ? 1 : 0
-  name        = module.this.id
+  name        = var.elasticache_subnet_groupName
   description = "Elasticache subnet group for ${module.this.id}"
   subnet_ids  = var.subnets
   tags        = module.this.tags
@@ -90,7 +90,7 @@ resource "aws_elasticache_subnet_group" "default" {
 
 resource "aws_elasticache_parameter_group" "default" {
   count       = module.this.enabled ? 1 : 0
-  name        = var.elasticache_parameter_group
+  name        = var.elasticache_parameter_groupName
   description = var.parameter_group_description != null ? var.parameter_group_description : "Elasticache parameter group for ${module.this.id}"
   family      = var.family
 
